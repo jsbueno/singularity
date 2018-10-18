d2305ce202214b163a41578976acf3771762c720
Joao S O Bueno <gwidion@gmail.com>
2018-10-17 22:44:24 -0300

    setup script

---
+++ setup.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name = 'singularity',
+    packages = ['singularity'],
+    version = "0.1",
+    license = "LGPLv3+",
+    author = "João S. O. Bueno",
+    author_email = "gwidion@gmail.com",
+    description = "Rich Data Tools - multi-serialiser and acessor package",
+    keywords = "data management",
+    py_modules = ['singularity'],
+    url = 'https://github.com/jsbueno/singularity',
+    long_description = open('README.md').read(),
+    test_requires = ['pytest'],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ]
+)
